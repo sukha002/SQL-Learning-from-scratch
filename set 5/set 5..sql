069cb534971f24fe6e2bf6188888cc59ac2c4ca0
Sukhwinder Singh <31208964+sukha002@users.noreply.github.com>
2023-06-05 19:16:21 -0400

    SUM AVG, COUNT, MAX, ORDERBY GROUP BY.pdf

---
+++ set 5/set 5..sql
@@ -0,0 +1,46 @@
+/*
+-- set 5
+select * from sys.databases;
+use LEarning;
+
+select * 
+From sys.tables;
+
+select * from emp;
+
+-- 1. print the dept name with the max salary;
+select dept, max(salary) as max_salary
+from emp
+group by dept
+
+
+-- 2. print the dept name, max salary in an ascending order.
+select dept, max(salary) as max_salary
+from emp
+group by dept
+order by max_salary asc
+
+-- 3. print the dept name, total salary in an descending order
+select dept, sum(salary) as total_salary
+from emp
+group by dept
+order by total_salary desc
+
+-- 4. print the dept name with the count of employee and the sum of salaries. 
+select dept,  count(dept) as count_employee, sum(salary) as total_salaries 
+from emp
+group by dept
+order by count_employee
+
+-- 5. print dept name with an average salary
+select dept, avg(salary) as dept_avg
+from emp
+group by dept
+*/
+
+-- 6. write the dept having employee more than 1
+select dept, count(dept) as employee
+from emp
+group by dept
+HAving count(emp.dept) > 1
+
